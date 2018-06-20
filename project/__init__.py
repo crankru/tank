decef3adc7e5c68c4e14483cb5480f74d99c9669
Crank <crank@crank.ru>
2018-06-20 23:39:17 +0300

    bug fix

--- project/__init__.py
+++ project/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask_socketio import SocketIO
 # from Queue import Queue
 
-import eventlet
-eventlet.monkey_patch()
-
-# from gevent import monkey
-# # see: https://github.com/miguelgrinberg/Flask-SocketIO/issues/65
-# monkey.patch_all()
-
 from project import config
+
+if not config.SEPARATE_STREAM_PROCESS:
+    import eventlet
+    eventlet.monkey_patch()
+
+    # from gevent import monkey
+    # # see: https://github.com/miguelgrinberg/Flask-SocketIO/issues/65
+    # monkey.patch_all()
 
 app = Flask(__name__)
 app.config.from_object(config)
