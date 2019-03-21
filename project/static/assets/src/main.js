ac9deb0a5cb62edf765026670a8cc2cfff6d79aa
Crank <crank@crank.ru>
2019-03-22 01:10:40 +0300

    vuejs

--- project/static/assets/src/main.js
+++ project/static/assets/src/main.js
@@ -1,1 +1,11 @@
-console.log('test');
+var App = {
+    template: '<div class="container"> \
+        <div class="row">test1111</div> \
+    </div>',
+}
+
+new Vue({
+    el: '#app',
+    components: { App },
+    template: '<App />',
+});
